71a239911e538dfc6414e8ac6bc347b92984e74c
Mati Tsiplakov <mati.tsiplakov@gmail.com>
2023-09-07 02:26:58 +0300

    Changed server.js to index.js and updated base URL

--- client/src/index.js
+++ client/src/index.js
@@ -10,7 +10,7 @@ import { StoreProvider } from "./Context/Store";
 // axios.defaults.baseURL = "http://localhost:5000/api";
 axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL
   ? process.env.REACT_APP_API_BASE_URL
-  : "https://netflix-clone-server-three.vercel.app/api";
+  : "eshop-mern-server.vercel.app/api";
 
 // const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(
